64cb42b6780bbfd5e4ab5def03a1268abd7049bd
jymmak <henryfrancisco_fernando@hotmail.com>
2018-01-28 00:42:09 -0500

    subiendo test js

---
+++ src/test.js
@@ -0,0 +1,13 @@
+let cardify = require('../src/main.js')
+describe('cardify', () => {
+  it(' deberia buscar todas las imagenes dentro de un contendor', () => {
+
+  });
+  it('deberia reemplazar las imagenes por elemento <figure>', () => {
+
+  });
+  it('deberia tener un <figcaption> con el texto del atri7buto alt', () => {
+
+  });
+
+});
