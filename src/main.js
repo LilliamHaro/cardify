d7969379afea742471b9b4e7bb7ac9ff73f8ddc6
LilliamHaro <qualia950@gmail.com>
2018-01-27 20:45:28 -0500

    subiendo funcion cardify normal

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
-let firstTest = 'hello';
-let secondTest = 'goodbye'
+let cardify = (containerID) => {
+  let container = document.getElementById(containerID);
+  let images = container.querySelectorAll('img');
+  images.forEach((img) => {
+    let figure = document.createElement('figure');
+    let figCaption = document.createElement('figcaption');
+    let altText = img.getAttribute('alt');
+    var imgCopy = img;
+    figCaption.innerText = altText;
+    img.replaceWith(figure);
+    figure.appendChild(imgCopy);
+    figure.append(figCaption);
+  });
+};
