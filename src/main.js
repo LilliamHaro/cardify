6496cd84dd891614c45190d61e7fa1ae383b81dc
jymmak <henryfrancisco_fernando@hotmail.com>
2018-01-29 11:47:01 -0500

    mejorando main.js

--- src/main.js
+++ src/main.js
@@ -20,15 +20,23 @@
   // fn$.fn => jQuery.prototype
   $.fn.cardify = function() {
     let images = $(this).find('img');
+    $('img').css({ 'width': '80p%',
+    'heigth': '40%' });
     images.each(function() {
       // encerrando la imagen en la etiqueta figure
-      $(this).wrap('<figure>');
+      $(this).wrap('<figure></figure>').css({'margin-top': '15%',
+      'margin-left': '10%',
+      'box-shadow': '10px 10px white, -10px -10px rgb(132, 175, 155), 0px 0px 40px 10px pink', 
+      'border-color': '1px solid blue' });
       // creando la etiqueta figcaption con el contenido del attr alt de la imagen
       let figcaption = $('<figcaption>');
       let altText = $(this).attr('alt');
-      figcaption.text(altText);
+      figcaption.text(altText).css({ 'color': 'darkblue',
+      'font-size': '2rem',
+      'margin-left': '10%',
+      'margin-top': '5%'});
       // anexando la etiqueta figcaption y su contenido a la etiqueta figure con la imagen
-      $(this).parent().append(figcaption);
+      $(this).parent().append($('<figcaption></figcaption>'));
       // return this;
     });
   };
