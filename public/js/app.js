b25a5b564ca442b12f4bb14e05b9fd56723d17c4
LilliamHaro <qualia950@gmail.com>
2018-02-04 19:00:37 -0500

    corrigiendo detalles del code-review

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,8 @@
-
 $(document).ready(() => {
-  $('.overflow').cardify();
+  $('#withCardify').on('click', function(event) {
+    $('.overflow').cardify();
+  });
+  $('#withoutCardify').on('click', function(event) {
+    window.location.href = '../public/index.html';
+  });
 });
